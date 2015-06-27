c0ad9fa6020077e095335ad4dc727cde2995accd
Van-Duyet Le <ubuntu@ubuntu-virtual-machine.(none)>
2015-06-28 00:27:37 +0700

    Add Agent Campaign to BGateAgent

--- helper/BgateAgent.js
+++ helper/BgateAgent.js
@@ -20,7 +20,8 @@ var BGateAgent = {
 
 		BGateAgent.agents = []; // reset empty
 
-		var q = "SELECT * FROM (SELECT * FROM auth_Users WHERE DemandCustomerInfoID IS NOT NULL) AS tb1 INNER JOIN (SELECT * FROM `DemandCustomerInfo`) AS tb2 ON tb1.DemandCustomerInfoID = tb2.DemandCustomerInfoID INNER JOIN (SELECT * FROM `AdCampaignBannerPreview`) AS tb3 ON tb1.user_id = tb3.UserID;";
+		//var q = "SELECT * FROM (SELECT * FROM auth_Users WHERE DemandCustomerInfoID IS NOT NULL) AS tb1 INNER JOIN (SELECT * FROM `DemandCustomerInfo`) AS tb2 ON tb1.DemandCustomerInfoID = tb2.DemandCustomerInfoID INNER JOIN (SELECT * FROM `AdCampaignBannerPreview`) AS tb3 ON tb1.user_id = tb3.UserID;";
+		var q = "SELECT * FROM (SELECT * FROM auth_Users WHERE DemandCustomerInfoID IS NOT NULL) AS tb1 INNER JOIN (SELECT * FROM `DemandCustomerInfo`) AS tb2 ON tb1.DemandCustomerInfoID = tb2.DemandCustomerInfoID INNER JOIN (SELECT * FROM `AdCampaignBannerPreview`) AS tb3 ON tb1.user_id = tb3.UserID INNER JOIN (SELECT AdCampaignPreviewID As AdCampaignID, UserID, `Name` As CampaignName, CampaignMarkup, StartDate as CampaignStartDate, EndDate As CampaignEndDate, ImpressionsCounter as CampaignImpressionsCounter, MaxImpressions as CampaignMaxImpressions, CurrentSpend as CampaignCurrentSpend, MaxSpend as CampaignMaxSpend, CPMTarget as CampaignCPMTarget, CPMTargetValue as CampaignCPMTargetValue, Active as CampaignActive, Deleted as CampaignDeleted, DateCreated as CampaignDateCreated, DateUpdated as CampaignDateUpdated, ChangeWentLive as CampaignChangeWentLive, WentLiveDate as CampaignWentLiveDate, Approval as CampaignApproval FROM AdCampaignPreview) AS tb4 ON (tb3.UserID = tb4.UserID AND tb3.AdCampaignPreviewID = tb4.AdCampaignID);";
 
 		connection.query(q, function(err, rows, fields) {
 			if (err || !rows) throw err;
@@ -40,40 +41,85 @@ var BGateAgent = {
 
 				if (!isExists) {
 					var agent = {
-						UserID: row.user_id,
-						user_email: row.user_email,
-						user_enabled: row.user_enabled,
-						user_verified: row.user_verified,
-						user_agreement_accepted: row.user_agreement_accepted,
-						locale: row.locale,
-						Name: row.Name,
-						Website: row.Website,
-						Company: row.Company,
-						PartnerType: row.PartnerType,
-						Balance: row.Balance,
-						banner: []
+						UserID 					: row.user_id,
+						user_email 				: row.user_email,
+						user_enabled 			: row.user_enabled,
+						user_verified 			: row.user_verified,
+						user_agreement_accepted	: row.user_agreement_accepted,
+						locale					: row.locale,
+						Name 					: row.Name,
+						Website 				: row.Website,
+						Company 				: row.Company,
+						PartnerType 			: row.PartnerType,
+						Balance 				: row.Balance,
+						banner 					: [], 
+						campaign 				: []
 					};
 
+					// Load campaign
+					
+					var agentCampaign = [];
+					for (var jjj in rows) {
+						if (rows[jjj].user_id == row.user_id) {
+							var _isExistsCampaign = false;
+							agentCampaign.forEach(function(campaign) {
+								if (_isExistsCampaign == true) return false;
+								if (campaign.AdCampaignID == rows[jjj].AdCampaignID) _isExistsCampaign = true;
+							});
+
+							if (!_isExistsCampaign) {
+								var _rowCampaign = rows[jjj];
+
+								var campaign = initCampainAttributes({
+									AdCampaignID 					: _rowCampaign.AdCampaignID,
+									CampaignName 					: _rowCampaign.CampaignName,
+									CampaignMarkup 					: _rowCampaign.CampaignMarkup,
+									CampaignStartDate 				: _rowCampaign.CampaignStartDate,
+									CampaignEndDate 				: _rowCampaign.CampaignEndDate,
+									CampaignImpressionsCounter 		: _rowCampaign.CampaignImpressionsCounter,
+									CampaignMaxImpressions 			: _rowCampaign.CampaignMaxImpressions,
+									CampaignCurrentSpend 			: _rowCampaign.CampaignCurrentSpend,
+									CampaignMaxSpend 				: _rowCampaign.CampaignMaxSpend,
+									CampaignCPMTarget 				: _rowCampaign.CampaignCPMTarget,
+									CampaignCPMTargetValue			: _rowCampaign.CampaignCPMTargetValue,
+									CampaignActive 					: _rowCampaign.CampaignActive,
+									CampaignDeleted 				: _rowCampaign.CampaignDeleted,
+									CampaignDateCreated 			: _rowCampaign.CampaignDateCreated,
+									CampaignDateUpdated 			: _rowCampaign.CampaignDateUpdated,
+									CampaignChangeWentLive 			: _rowCampaign.CampaignChangeWentLive,
+									CampaignWentLiveDate 			: _rowCampaign.CampaignWentLiveDate,
+									CampaignApproval 				: _rowCampaign.CampaignApproval
+								});
+
+								if (passSelfCampaignFilter(campaign)) {
+									agentCampaign.push(campaign);
+								}
+							}
+						}
+					}
+					agent.campaign = agentCampaign;
+
+					// Load banner 
 					for (var j in rows) {
 						//console.log("==== GET BANNER OF "+ i +" ====");
 						// Check banner is in array 
 						if (rows[j].user_id == row.user_id) {
 							//console.log("PASS USER ", row.user_id);
 							var _isExists = false;
-							for (var z in agent.banner) {
+							agent.banner.forEach(function(banner) {
 								if (_isExists == true) return false;
-								if (agent.banner[z].AdCampaignBannerPreviewID == rows[j].AdCampaignBannerPreviewID) {
+								if (banner.AdCampaignBannerPreviewID == rows[j].AdCampaignBannerPreviewID) {
 									_isExists = true;
-									return false;
+									// return false;
 								}
-							}
+							});
 
 							if (!_isExists) {
 								
 								var _rowBanner = rows[j];
-								var banner = {
+								var banner = initBannerAttributes({
 									AdCampaignBannerPreviewID: _rowBanner.AdCampaignBannerPreviewID,
-									AdCampaignPreviewID: _rowBanner.AdCampaignPreviewID,
+									AdCampaignID: _rowBanner.AdCampaignID,
 									ImpressionType: _rowBanner.ImpressionType,
 									Name: _rowBanner.Name, 
 									StartDate: _rowBanner.StartDate, 
@@ -125,19 +171,18 @@ var BGateAgent = {
 									PmpEnable: _rowBanner.PmpEnable,
 									Secure: _rowBanner.Secure,
 									FoldPosition: _rowBanner.FoldPosition,
-								};
+								});
 
-								banner = initBannerAttributes(banner);
-
-								if (passSelfBannerFilter(banner))
+								if (passSelfBannerFilter(banner)) {
 									agent.banner.push(banner);
+								}
 
 							}
 						}
-					} 
+					}
 
 					BGateAgent.agents.push(agent);
-				} 
+				}
 			});
 			// console.log(BGateAgent.agents);
 
@@ -161,6 +206,18 @@ var passSelfBannerFilter = function(banner) {
 		if (moment().diff(endDate, "seconds") > 0) return false; 
 	}
 
+	// Check campaign status, if campaign is disable ==> disable me
+	var campaign = getCampaignById(banner.AdCampaignID);
+	if (!campaign) {
+		// not found campaign, opp
+		console.error("passSelfBannerFilter: not found campaign ", banner.AdCampaignID);
+		// return false;
+	}
+	if (!campaign.CampaignActive || campaign.CampaignActive == 0) {
+		banner.BannerActive = 0;
+		// If return false here ==> do not add this banner to agents
+	}
+
 	return true;
 };
 
@@ -177,9 +234,49 @@ var initBannerAttributes = function(banner) {
 		}
 	}
 
+	// Banner status 
+	banner.BannerActive = 1;
+	banner.BannerActiveStatus = "";
+
 	// TODO: All attributes for banner, ex: todayImp, totalImp, clickCounter, ...
 	return banner;
 };
+
+var passSelfCampaignFilter = function(campaign) {
+	// TODO: Check campagin conditions
+
+	return true;
+}
+
+var initCampainAttributes = function(campaign) {
+	// TODO: Add campaign attr
+
+	return campaign;
+}
+
+// ----------------
+var getCampaignById = function(campaignId) {
+	if (!BGateAgent || !BGateAgent.agents) return false;
+	var isFounded = false;
+	var result = {};
+
+	BGateAgent.agents.forEach(function(agent) {
+		// console.error(agent);
+		if (isFounded) return false;
+		if (!agent.campaign) return false;
+
+		agent.campaign.forEach(function(campaign) {
+			if (isFounded) return false;
+			if (campaign.AdCampaignID == campaignId) {
+				isFounded = true;
+				result = campaign;
+			}
+		});
+	});
+
+	if (isFounded) return result;
+	return false;
+}
 
 
 BGateAgent.init(function() {
